2926a62cf179cce0690c6d595c9c6c9f7747e1e5
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2019-03-01 11:50:06 -0800

    update CuBLM function
    When matching Cu data to it's ancillary data, use the first result found in the calendar day as the Cu sample.

--- R/CuBLM_Transform.R
+++ R/CuBLM_Transform.R
@@ -1,16 +1,19 @@
 #' CuBLM
 #'
 #' A function that converts data from AWQMS long table format into the wide table format that
-#  is used by the copper BLM model and the NPDES Copper BLM templates. if you get warnings with
-#' res saying that multiple rows match, look for duplicates in data. Written by Aliana Britson
+#  is used by the copper BLM model and the NPDES Copper BLM templates. Ancillary data is
+#' the first data found in the calendar day. This is the same method used in 2018IR
+#' Written by Aliana Britson
 #'
 #' @param x table output from AWQMS query
 #' @return Dataframe with input file for BLM
+#' @importFrom magrittr "%>%"
 #' @export
 #'
 #'
 
-CuBLM<-function(x) {
+
+CuBLM <- function(x) {
   #x is table output from AWQMS query
 
   #Remove rejected data
@@ -34,24 +37,49 @@ CuBLM<-function(x) {
   #can leave out the other Sample Fractions
   y$Char_Name<-
     dplyr::case_when(y$Char_Name %in% c("Calcium","Copper","Magnesium","Potassium","Sodium","Organic carbon")
-              ~paste0(y$Char_Name,",",y$Sample_Fraction),
-              y$Char_Name %in% c("Alkalinity, total","Chloride","pH","Sulfate","Temperature, water","Total Sulfate","Sulfide","Salinity","Conductivity")
-              ~y$Char_Name)
+                     ~paste0(y$Char_Name,",",y$Sample_Fraction),
+                     y$Char_Name %in% c("Alkalinity, total","Chloride","pH","Sulfate","Temperature, water","Total Sulfate","Sulfide","Salinity","Conductivity")
+                     ~y$Char_Name)
+
+  # Get only ancillary data
+  ancillary <- y[!grepl("Copper", y$Char_Name), ]
+  #Set date
+  ancillary$date <- as.Date(ancillary$SampleStartDate)
+
+  #Pare down dataframe to only columns we need
+  ancillary<-subset(ancillary,select=c("Char_Name","Result","date","OrganizationID","MLocID"))
+
+  # Choose the first result in a day
+  # transform data from long to wide
+  ancillary <- ancillary %>%
+    dplyr::group_by(Char_Name, date, OrganizationID, MLocID) %>%
+    dplyr::summarise(Result = dplyr::first(Result)) %>%
+    dplyr::ungroup() %>%
+    tidyr::spread(key = Char_Name, value =Result )
+
 
 
   #just want a subset of the columns, too many columns makes reshape very complicated
-  x<-subset(y,select=c("Char_Name","Result_Numeric","SampleStartDate","SampleStartTime","OrganizationID","MLocID","Project1"))
+  x<-subset(x,select=c("Char_Name","Result","SampleStartDate","SampleStartTime","OrganizationID","MLocID","Project1"))
 
   colnames(x) <- c("Char_Name","Result","SampleStartDate","SampleStartTime","OrganizationID","MLocID","Project1")
 
-  res<-reshape(x, timevar="Char_Name",
-               idvar=c("MLocID","SampleStartDate","SampleStartTime","OrganizationID","Project1"),
-               direction="wide")
 
-  #note, if you get warnings with res saying that multiple rows match, look for duplicates in data
+  #Get only copper data
+  Copper <- y[grepl("Copper", y$Char_Name), ]
 
-  return(res)
+  Copper<-subset(Copper,select=c("Char_Name","Result","SampleStartDate","SampleStartTime","OrganizationID","MLocID","Project1"))
+
+  # Join copper data to ancillary data
+  Copper_joined <- Copper %>%
+    dplyr::mutate(date = as.Date(SampleStartDate)) %>%
+    dplyr::left_join(ancillary, by = c('date', 'OrganizationID', 'MLocID'))
+
+
+
+  return(Copper_joined)
 }
+
 
 
 
